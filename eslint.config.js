f59859decd8519ff6887356f7979c38511bf23da
igmrsky <mrinskiy0282@mail.ru>
2024-05-17 18:29:15 +0400

    added eslint, prettier, stylelint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+    {
+        plugins: {
+            prettier: prettierPlugin,
+        },
+    },
+    {
+        ignores: ['node_modules', 'dist'],
+    },
+    js.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+                ...globals.es2021,
+            },
+        },
+    },
+    {
+        files: ['**/*.{js, jsx}'],
+        rules: {
+          ...eslintConfigPrettier.rules,
+          'prettier/prettier': 'error'
+        }
+    },
+];
