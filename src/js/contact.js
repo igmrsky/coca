f23dab28e1c5e88ab779f8585aa3713f6339b115
igmrsky <mrinskiy0282@mail.ru>
2024-06-14 19:36:46 +0500

    added contact: promotion

--- src/js/contact.js
+++ src/js/contact.js
@@ -6,9 +6,11 @@ import { useHeaderScroll } from './components/scroll.js';
 import { useBurger } from './components/burger.js';
 import { useFooterAccordion } from './components/footer-accordion.js';
 import { useMap } from './components/contact/map.js';
+import { usePhone } from './components/contact/phone.js';
 
 useTheme();
 useHeaderScroll();
 useBurger();
 useFooterAccordion();
 useMap();
+usePhone();
